0983f6238f3b6fe5fa8fdd2c18f8966b9dc0841b
Legich <sorokin.oleg@gmail.com>
2022-11-18 14:42:45 +0300

    small code update & save activity instance.

--- app/src/main/java/ru/fastly/clicker/MainActivity.kt
+++ app/src/main/java/ru/fastly/clicker/MainActivity.kt
@@ -1,14 +1,9 @@
 package ru.fastly.clicker
 
-import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.view.View.OnLongClickListener
-import android.widget.Button
 import android.widget.TextView
-import android.content.SharedPreferences
-import android.content.SharedPreferences.Editor
+import androidx.appcompat.app.AppCompatActivity
+import ru.fastly.clicker.databinding.ActivityMainBinding
 
 
 
@@ -16,96 +11,93 @@ class MainActivity : AppCompatActivity() {
 
     private var leftSum = 0
     private var rightSum = 0
-    val PREF_KEY = "storage"
-    val PREF_LEFT = "left"
-    val PREF_RIGHT = "right"
-
-    lateinit var appSetting:SharedPreferences
+    private val LEFT_SIDE = "left"
+    private val RIGHT_SIDE = "right"
+    private lateinit var binding: ActivityMainBinding
 
 
-    fun saveCounters() {
-     val editor:Editor =  appSetting.edit()
-     editor.putInt(PREF_LEFT,leftSum)
-     editor.putInt(PREF_RIGHT, rightSum)
-     editor.apply()
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt(LEFT_SIDE, leftSum)
+        savedInstanceState.putInt(RIGHT_SIDE, rightSum)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
        super.onCreate(savedInstanceState)
 
-        appSetting = getSharedPreferences(PREF_KEY, Context.MODE_PRIVATE)
-        leftSum = appSetting.getInt(PREF_LEFT, 0)
-        rightSum = appSetting.getInt(PREF_RIGHT, 0)
+
+        leftSum = savedInstanceState?.getInt(LEFT_SIDE,0) ?:0
+        rightSum = savedInstanceState?.getInt(RIGHT_SIDE,0) ?:0
 
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val leftMinus = findViewById<Button>(R.id.leftMinus)
-        val leftPlus = findViewById<Button>(R.id.leftPlus)
-        val rightMinus = findViewById<Button>(R.id.rightMinus)
-        val rightPlus = findViewById<Button>(R.id.rightPlus)
-        val leftCounter = findViewById<TextView>(R.id.leftCounter)
-        val rightCounter = findViewById<TextView>(R.id.rightCounter)
+        val leftMinusButton = binding.leftMinus
+        val leftPlusButton = binding.leftPlus
+        val rightMinusButton = binding.rightMinus
+        val rightPlusButton = binding.rightPlus
+        val leftCounterText = binding.leftCounter
+        val rightCounterText = binding.rightCounter
 
-        leftCounter.text = leftSum.toString()
-        rightCounter.text = rightSum.toString()
+        leftCounterText.text = leftSum.toString()
+        rightCounterText.text = rightSum.toString()
 
-        leftMinus.setOnClickListener {
-           setClick("left",'-', leftCounter)
+        leftMinusButton.setOnClickListener {
+           setClick(LEFT_SIDE,'-', leftCounterText)
         }
 
         //удержание кнопки трет счетчик
-        leftMinus.setOnLongClickListener(View.OnLongClickListener {
-            clearCounter("left", leftCounter)
-        })
-
-        leftPlus.setOnClickListener{
-            setClick("left",'+', leftCounter)
+        leftMinusButton.setOnLongClickListener {
+            clearCounter(LEFT_SIDE, leftCounterText)
         }
 
-        rightMinus.setOnClickListener {
-            setClick("right",'-', rightCounter)
+        leftPlusButton.setOnClickListener{
+            setClick(LEFT_SIDE,'+', leftCounterText)
+        }
+
+        rightMinusButton.setOnClickListener {
+            setClick(RIGHT_SIDE,'-', rightCounterText)
         }
 
         //удержание кнопки трет счетчик
-        rightMinus.setOnLongClickListener(View.OnLongClickListener {
-            clearCounter("right", rightCounter)
-        })
+        rightMinusButton.setOnLongClickListener{
+            clearCounter(RIGHT_SIDE, rightCounterText)
+        }
 
-        rightPlus.setOnClickListener {
-            setClick("right",'+', rightCounter)
+        rightPlusButton.setOnClickListener {
+            setClick(RIGHT_SIDE,'+', rightCounterText)
         }
 
     }
 
-    fun clearCounter(side:String, view: TextView):Boolean {
-        if (side == "left") {
+    private fun clearCounter(side:String, view: TextView):Boolean {
+        if (side == LEFT_SIDE) {
             leftSum = 0
             view.text = leftSum.toString()
         } else {
             rightSum = 0
             view.text = rightSum.toString()
         }
-        saveCounters()
         return true
     }
 
-
-    fun setClick(side:String, action:Char, view: TextView){
-        if (side == "left") {
+    private fun setClick(side:String, action:Char, view: TextView){
+        if (side == LEFT_SIDE) {
            when (action) {
-               '-' -> if (leftSum < 1) 0 else leftSum--
+               '-' -> if (leftSum > 0) leftSum--
                '+' -> leftSum++
                }
             view.text = leftSum.toString()
             }
          else {
             when (action) {
-                '-' -> if (rightSum < 1) 0 else rightSum--
+                '-' -> if (rightSum > 0)  rightSum--
                 '+' -> rightSum++
             }
             view.text = rightSum.toString()
         }
-        saveCounters()
     }
+
 }
+
